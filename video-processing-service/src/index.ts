4603a1b569e83d5cdfec5e2a202bc14a0ae14e9a
chenchiaho <chenchiaho@gmail.com>
2024-05-17 19:30:57 +0800

    implement video scaling to 360p

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,40 @@
 import express from 'express'
+import Ffmpeg from 'fluent-ffmpeg'
+import fs from 'fs'
 
 const app = express()
-const port = 3000
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.post('/process-video', (req, res) => {
+    const inputFilePath = req.body.inputFilePath
+    const outputFilePath = req.body.outputFilePath
+
+    if (!inputFilePath || !outputFilePath) {
+        return res.status(400).send('Bad Request: Missing file path.')
+    }
+
+    if (!fs.existsSync(inputFilePath)) {
+        return res.status(400).send('Bad Request: Input file does not exist.')
+    }
+
+    Ffmpeg(inputFilePath)
+        .outputOptions('-vf', 'scale=-2:360')
+        .on('start', (commandLine) => {
+            console.log(`Spawned FFmpeg with command: ${commandLine}`)
+        })
+        .on('end', () => {
+            console.log('Processing finished successfully.')
+            res.status(200).send('Processing finished successfully.')
+        })
+        .on('error', (err) => {
+            console.error(`Internal Server Error: ${err.message}`)
+            res.status(500).send(`Internal Server Error: ${err.message}`)
+        })
+        .save(outputFilePath)
 })
 
+const port = process.env.PORT || 3000
+
 app.listen(port, () => {
-    console.log(`listening at ${port}`)
+    console.log(`Listening at http://localhost:${port}`)
 })
