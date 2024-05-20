d9cab274e334200f29c01fd7850b90b0ca81fd86
chenchiaho <chenchiaho@gmail.com>
2024-05-20 14:37:30 +0800

    create next app

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -63,7 +63,7 @@ app.post('/process-video', async (req, res) => {
     return res.status(200).send('Processing finished successfully');
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
