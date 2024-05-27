f96b63cce6b8402ea0bffca2c780b09e0762c9fa
chenchiaho <chenchiaho@gmail.com>
2024-05-27 20:07:54 +0800

    add video name

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -3,21 +3,28 @@ import Link from 'next/link'
 import { getVideos } from './firebase/functions'
 import styles from './page.module.css'
 
-
 export default async function Home() {
   const videos = await getVideos()
   videos.forEach(video => console.log(video))
+
   return (
-    <main>
-      {
-        videos.map((video) => (
-          <Link href={`/watch?v=${video.filename}`} key={video.id}>
-            <Image src={'/thumbnail.png'} alt='video' width={120} height={80}
-              className={styles.thumbnail} />
+    <div className={styles.main}>
+      {videos.map((video) => {
+        const fileName = video?.filename?.split('-').slice(2).join('-').split('.')[0];
+        return (
+          <Link href={`/watch?v=${video.filename}`} key={video.id} className={styles.videoContainer}>
+            <Image
+              src={'/thumbnail.png'}
+              alt='video'
+              width={120}
+              height={80}
+              className={styles.thumbnail}
+            />
+            {fileName}
           </Link>
-        ))
-      }
-    </main>
+        )
+      })}
+    </div>
   )
 }
 
